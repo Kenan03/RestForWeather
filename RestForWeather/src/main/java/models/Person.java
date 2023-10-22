7307730c33b97b4ead414da67b0246485e6817e9
Kenan Agashirinov <71469048+Kenan03@users.noreply.github.com>
2023-10-22 15:42:56 +0300

    Add files via upload

---
+++ RestForWeather/src/main/java/models/Person.java
@@ -0,0 +1,48 @@
+package models;
+
+public class Person {
+    int id;
+
+    public String name;
+    public String email;
+    public String password;
+
+    public Person(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
